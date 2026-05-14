412a23321dbce6dd23761f877831436d124a1b6e
feor <mistilplay558@gmail.com>
2026-05-14 19:48:11 +0500

    complete pm5-lw2

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log('Happy developing ✨')
+import chalk from 'chalk';
+
+console.log(chalk.green('Программа запущена'));
+console.log(chalk.yellow(`Платформа: ${process.platform}`));
+console.log(chalk.red.bold('Программа завершена'));
---
+++ validator.js
@@ -0,0 +1,7 @@
+export function isPositive(n) {
+  return n >= 1;
+}
+
+export function isEven(n) {
+  return n % 2 === 0;
+}
